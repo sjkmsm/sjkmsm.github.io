dab5800f876ef64207b646ec67ee7591632dfa32
sjkmsm <gomashio.kinako28@gmail.com>
2021-12-25 16:57:48 +0900

    test push

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import me from "./assets/me.jpeg";
+import {
+  Avatar,
+  Container,
+  createTheme,
+  CssBaseline,
+  Grid,
+  Theme,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import { createStyles, makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    title: {
+      margin: theme.spacing(3),
+    },
+  })
+);
+const theme = createTheme({
+  typography: {
+    fontFamily: "Sawarabi Mincho",
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <Grid container>
+            <Grid item xs={12}>
+              <Typography
+                className={classes.title}
+                variant="h3"
+                component="div"
+              >
+                h3. Heading
+              </Typography>
+            </Grid>
+            <Grid container item xs={12}>
+              <Grid item xs="auto">
+                <Avatar alt="ごましおきなこ" src={me} />
+              </Grid>
+              <Grid item xs>
+                <Typography variant="body2" component="div">
+                  書いた人:ごましおきなこ
+                </Typography>
+                <Typography variant="body2" component="div">
+                  社会人もうすぐ5年目、転職してもうすぐ2年経ちます。
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body1" component="div">
+                この記事は
+                <a
+                  href="https://adventar.org/calendars/6457"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  はじめてのIT勉強会アドベントカレンダー'21
+                </a>
+                の25日目の記事です。
+              </Typography>
+            </Grid>
+          </Grid>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
